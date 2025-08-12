9b99bce0c9d9c3eb5c7e791f6c19054b453386a0
Vedant Mishra <mishrasandhya0364@gmail.com>
2025-08-12 14:25:39 +0530

    best time to buy and sell stocks

--- Day_1/LC_121_Best_Time_to_Buy_and_Sell_Stock.java
+++ Day_1/LC_121_Best_Time_to_Buy_and_Sell_Stock.java
@@ -2,6 +2,19 @@ package Day_1;
 
 public class LC_121_Best_Time_to_Buy_and_Sell_Stock {
     public static void main(String[] args) {
-        
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println(maximumProfit(prices));
+    }
+
+    static int maximumProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for(int i = 0; i < prices.length; i++) {
+            minPrice = Math.min(minPrice, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        }
+
+        return maxProfit;
     }
 }
